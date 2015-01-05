4679f520d8b3fd357e6296224be149349bce86d4
AMGalvanS2 <amgalvancollege@gmail.com>
2015-01-05 00:57:45 -0600

    Create ExamplePuppy1.java

---
+++ Puppy/ExamplePuppy1.java
@@ -0,0 +1,18 @@
+public class ExamplePuppy1{ //Does not work if you use (1) simply delete (1) before programming
+   
+    public ExamplePuppy1(){ //Does not work if you use (1) simply delete (1) before programming
+   }
+
+   public ExamplePuppy1(String name){
+      // This constructor has one parameter, name.
+      
+   /* First create an object */
+   ObjectReference = new Constructor();
+
+   /* Now call a variable as follows */
+   ObjectReference.variableName;
+
+  /* Now you can call a class method as follows */
+  ObjectReference.MethodName();    
+   }
+}
