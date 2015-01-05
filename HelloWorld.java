8abb7da391ab242622a87fddb1f0f83febaab38b
AMGalvanS2 <amgalvancollege@gmail.com>
2015-01-04 23:14:02 -0600

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) { 
-        System.out.println("Hello, World! My name is Andrea"); //Line 1
-    } //End of main
-} //End of HelloWorld Class
